f33f9e01755234f52f1730ca49f87fe1c1c2ea76
ahz-r3v <ahz-r3v@ahz-r3vdeMacBook-Pro.local>
2023-10-22 22:26:02 +0800

    code review

--- player_dummy/player_dummy.gd
+++ player_dummy/player_dummy.gd
@@ -1,20 +1,21 @@
 extends CharacterBody2D
 
 @onready var playerAni = $AnimatedSprite2D		#角色动画
-@export var speed = 400
+@export var speed = 500
 
 const SCALE = 96
 const POSITION_FIX_X = 48
 const POSITION_FIX_Y = 48
-var initPos = Vector2(12,3)
+const initPos = Vector2(12,3)
 var canMove = false
 var destination = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	initPos[0] = initPos[0] * SCALE + POSITION_FIX_X
-	initPos[1] = initPos[1] * SCALE + POSITION_FIX_Y
-	position = initPos
+	var dest = Vector2(0, 0)
+	dest[0] = initPos[0] * SCALE + POSITION_FIX_X
+	dest[1] = initPos[1] * SCALE + POSITION_FIX_Y
+	position = dest
 	pass # Replace with function body.
 
 
@@ -26,8 +27,8 @@ func _process(delta):
 	
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		destination[0] = floor(get_global_mouse_position()[0] / 96)
-		destination[1] = floor(get_global_mouse_position()[1] / 96)
+		destination[0] = floor(get_global_mouse_position()[0] / SCALE)
+		destination[1] = floor(get_global_mouse_position()[1] / SCALE)
 		canMove = true
 	pass	
 
@@ -40,9 +41,10 @@ func moveTo(destination):
 		move_and_slide()
 	else:
 		position = dest
+		canMove = false
 	pass
 	
-func normalAtack():
+func normalAttack():
 	pass
 	
 func magic():
