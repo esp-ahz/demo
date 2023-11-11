8bc8da1de1b727b966ee5b9ac63869bb8cfcba25
esphere-dumal <esphere@qq.com>
2023-11-11 22:48:37 +0800

    chore: render slots with given cards

--- Deck/deck.gd
+++ Deck/deck.gd
@@ -1,16 +1,47 @@
 extends Node2D
-var slot1_occupied = false
-var slot2_occupied = false
-var slot3_occupied = false
-var slot4_occupied = false
-var slot5_occupied = false
-var slot6_occupied = false
 
-# Called when the node enters the scene tree for the first time.
+var slot = preload("res://Deck/Slot/slot.tscn")
+
+const kSlotSize = 128
+const kSlotMagin = 10
+
+# exposed variables
+var cards = []
+var renderDeck = false
+
+# private variables
+var _slots = []
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+func _process(delta):
+	if renderDeck:
+		delete_children()
+		put_cards_into_slots()
+		for s in _slots:
+			add_child(s)
+		renderDeck = false
+
+func delete_children():
+	var children = get_children()
+	for c in children:
+		c.queue_free()
+	
+func put_cards_into_slots():
+	_slots.resize(cards.size())
+	
+	# TODO(hangcen): 渲染卡牌槽的边框
+	for i in range(cards.size()):
+		var s = slot.instantiate()
+		s.position = Vector2(i * (kSlotSize+kSlotMagin), 0)
+		
+		if cards[i] != null:
+			cards[i].position = Vector2(0, 0)
+			s.add_child(cards[i])
+		
+		_slots[i] = s
+
+func _render_deck():
+	renderDeck = true
+		
